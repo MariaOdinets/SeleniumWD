2135af29c98a3c27a6112072d67183a66550a74c
MariaOdinets <MariaOdinets@gmail.com>
2019-01-14 13:47:58 -0500

    Task 4

---
+++ Project4/UnitTestProject4/UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            Login();
+            //var element= driver.FindElement(By.CssSelector(".product.column.shadow.hover-light"));
+            //var sticker = element.FindElement(By.CssSelector(".sticker"));
+            //Assert.NotNull(sticker);
+
+            var elements = driver.FindElements(By.CssSelector(".product.column.shadow.hover-light"));
+            foreach (var element in elements)
+            {
+                var sticker = element.FindElements(By.CssSelector(".sticker"));
+                Assert.NotNull(sticker);
+            }
+
+
+          
+
+
+        }
+
+
+        private void Login()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/en/rubber-ducks-c-1/");
+            
+        }
+            
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
